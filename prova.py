5446548b325dadcb0966bd6f0a056269235dccfc
marcoferri <m.ferri17@campus.unimib.it>
2019-11-25 16:42:47 +0100

    Maybe working

--- prova.py
+++ prova.py
@@ -6,6 +6,9 @@ import numpy as np
 import tensorflow as tf
 import sys
 
+import tensorflow.python.util.deprecation as deprecation
+deprecation._PRINT_DEPRECATION_WARNINGS = False
+
 # --------------------------------
 
 # from google.colab import drive
@@ -20,7 +23,7 @@ with open('datasets/montecristo.txt', 'r') as f:
   book = f.read()
 
 book = book.lower()
-print(book)
+# print(book)
 
 # --------------------------------
 
@@ -29,22 +32,30 @@ from collections import Counter
 from collections import OrderedDict
 
 char_counts = Counter(book)
+# print(len(char_counts))
 
 # --------------------------------
 
-char_counts_byletter = OrderedDict(sorted(char_counts.items()))
-print(f'Characters count ordered alphabetically: {char_counts_byletter}')
-df_char_counts_byletter = pandas.DataFrame.from_dict(char_counts_byletter, orient='index')
-df_char_counts_byletter.plot(kind='bar')
+## Characters distribution
+# char_counts_byletter = OrderedDict(sorted(char_counts.items()))
+# print(f'Characters count ordered alphabetically: {char_counts_byletter}')
+# df_char_counts_byletter = pandas.DataFrame.from_dict(char_counts_byletter, orient='index')
+# df_char_counts_byletter.plot(kind='bar')
 
-top = 15
-print(f'Top {top} most common characters')
-char_counts.most_common()[:top]
+## Top characters
+# top = 15
+# print(f'Top {top} most common characters')
+# char_counts.most_common()[:top]
 
 # --------------------------------
+
+## Handle text to numerical conversion
 
 def text_to_num(text):
   return list(map(lambda x: ord(x), text))
+
+def num_to_text(nums):
+  return list(map(lambda x: chr(x), nums))
 
 # --------------------------------
 
@@ -71,16 +82,18 @@ def generate_batches(source, batch_size, sequence_length):
 
 # --------------------------------
 
-example_text = 'Mi chiamo Marco e sono un gattino.'
-example_num = text_to_num(example_text)
-print(example_num)
-print(generate_batches(example_num, 3, 2))
+## Little example
+# example_text = 'Mi chiamo Marco e sono un gattino.'
+# example_num = text_to_num(example_text)
+# print(example_num)
+# print(generate_batches(example_num, 3, 2))
 
 # --------------------------------
 
 batch_size = 16
 sequence_length = 256
 bts = generate_batches(book_to_num, batch_size, sequence_length)
+
 # print(len(book_to_num) / batch_size / sequence_length)
 print('Number of batches', len(bts)) # ceiling(len(text) / batch_size / sequence_length)
 print('Batch size',len(bts[0]))
@@ -88,34 +101,40 @@ print('Sequence length',len(bts[0][0]))
 
 # --------------------------------
 
-for i in range(len(bts)):
-  for j in range(batch_size):
-    if len(bts[i][j]) != 256:
-      print(len(bts[i][j]), i, j)
+## Just to notice that last batch is incomplete
+# for i in range(len(bts)):
+#   for j in range(batch_size):
+#     if len(bts[i][j]) != 256:
+#       print(len(bts[i][j]), i, j)
 
 # --------------------------------
 
-np.array(bts[:-1]).shape
+## Creating dataset
+
+bts = np.array(bts[:-1]) # removing last batch because incomplete
+print('bts shape: ' , bts.shape)
+
+data_X = bts
+data_Y = np.copy(data_X)
+
+for batch in range(np.shape(bts)[0]):
+  for sequence in range(np.shape(bts)[1]):
+    for character in range(np.shape(bts)[2] - 1):
+      data_Y[batch][sequence][character] = data_X[batch][sequence][character+1]
+    data_Y[batch][sequence][np.shape(bts)[2] - 1] = 0 # last character has no target
+
+print('data_X shape: ', data_X.shape)
+print('data_Y shape: ', data_Y.shape)
 
 # --------------------------------
+
+## Model parameters
 
 seed = 0
 tf.reset_default_graph()
 tf.set_random_seed(seed=seed)
 
-# Task parameters
-# TODO
-n = 3 # n-back
-k = 4 # Input dimension
-mean_length = 20 # Mean sequence length
-std_length = 5 # Sequence length standard deviation
-n_sequences = 512 # Number of training/validation sequences
-
-# Creating datasets
-random_state = np.random.RandomState(seed=seed)
-X_train, Y_train, lengths_train = nback_dataset(n_sequences, mean_length, std_length, n, k, random_state) # TODO
-
-X_val, Y_val, lengths_val = nback_dataset(n_sequences, mean_length, std_length, n, k, random_state)  # TODO
+k = len(char_counts) # Input dimension (unique characters in the text)
 
 # Model parameters
 hidden_units = 256 # Number of recurrent units
@@ -126,10 +145,12 @@ n_epochs = 5
 
 # --------------------------------
 
-# Model definition
+## Model definition
+
 X_int = tf.placeholder(shape=[None, None], dtype=tf.int64)
 Y_int = tf.placeholder(shape=[None, None], dtype=tf.int64)
-lengths = tf.placeholder(shape=[None], dtype=tf.int64)
+lengths = tf.placeholder(shape=None, dtype=tf.int64)
+lengths_list = list([lengths] * batch_size)
 
 batch_size = tf.shape(X_int)[0]
 max_len = tf.shape(X_int)[1]
@@ -137,35 +158,36 @@ max_len = tf.shape(X_int)[1]
 # One-hot encoding X_int
 X = tf.one_hot(X_int, depth=k) # shape: (batch_size, max_len, k)
 # One-hot encoding Y_int
-Y = tf.one_hot(Y_int, depth=2) # shape: (batch_size, max_len, 2)
+Y = tf.one_hot(Y_int, depth=k) # shape: (batch_size, max_len, k)
 
 # Recurrent Neural Network
-cell = tf.nn.rnn_cell.BasicRNNCell(num_units=hidden_units)
+basic_cell = tf.nn.rnn_cell.BasicRNNCell(num_units=hidden_units)
 
 # Long-Short Term Memory Neural Network
-# cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_units)
+rnn_layers = [tf.nn.rnn_cell.LSTMCell(size) for size in [256, 256]]
+multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
+lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_units)
 
-init_state = cell.zero_state(batch_size, dtype=tf.float32)
-
+init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
 
 # rnn_outputs shape: (batch_size, max_len, hidden_units)
-rnn_outputs, \
-    final_state = tf.nn.dynamic_rnn(cell, X, sequence_length=lengths, initial_state=init_state)
+# rnn_outputs, final_state = tf.nn.dynamic_rnn(basic_cell, X, sequence_length=lengths, initial_state=current_state) # ERROR
+rnn_outputs, final_state = tf.nn.dynamic_rnn(lstm_cell, X, sequence_length=lengths_list, initial_state=init_state)
 
 # rnn_outputs_flat shape: ((batch_size * max_len), hidden_units)
 rnn_outputs_flat = tf.reshape(rnn_outputs, [-1, hidden_units])
 
 # Weights and biases for the output layer
-Wout = tf.Variable(tf.truncated_normal(shape=(hidden_units, 2), stddev=0.1))
-bout = tf.Variable(tf.zeros(shape=[2]))
+Wout = tf.Variable(tf.truncated_normal(shape=(hidden_units, k), stddev=0.1))
+bout = tf.Variable(tf.zeros(shape=[k]))
 
-# Z shape: ((batch_size * max_len), 2)
+# Z shape: ((batch_size * max_len), k)
 Z = tf.matmul(rnn_outputs_flat, Wout) + bout
 
-Y_flat = tf.reshape(Y, [-1, 2]) # shape: ((batch_size * max_len), 2)
+Y_flat = tf.reshape(Y, [-1, k]) # shape: ((batch_size * max_len), k)
 
 # Creates a mask to disregard padding
-mask = tf.sequence_mask(lengths, dtype=tf.float32)
+mask = tf.sequence_mask(lengths_list, dtype=tf.float32)
 mask = tf.reshape(mask, [-1]) # shape: (batch_size * max_len)
 
 # Network prediction
@@ -187,27 +209,36 @@ train = optimizer.minimize(loss)
 
 # --------------------------------
 
+### Training
+print('\n\n TRAINING \n')
+
 session = tf.Session()
 session.run(tf.global_variables_initializer())
 
 for e in range(1, n_epochs + 1):
-    feed = {X_int: X_train, Y_int: Y_train, lengths: lengths_train}
-    l, _ = session.run([loss, train], feed)
-    print(f'Epoch: {e}. Loss: {l}.')
+  cs = session.run(init_state, {X_int: data_X[0], Y_int: data_Y[0]}) # initial state
 
-feed = {X_int: X_val, Y_int: Y_val, lengths: lengths_val}
-accuracy_ = session.run(accuracy, feed)
-print(f'Validation accuracy: {accuracy_}.')
+  for b in range(np.shape(data_X)[0]):
+    feed = {X_int: data_X[b], Y_int: data_Y[b], lengths: sequence_length, init_state.c: cs.c, init_state.h: cs.h}
+    l, _, cs = session.run([loss, train, final_state], feed)
+    print(f'Epoch {e}, Batch {b}. \t Loss: {l}')
+  
 
-# Shows first task and corresponding prediction
-xi = X_val[0, 0: lengths_val[0]]
-yi = Y_val[0, 0: lengths_val[0]]
-print('Sequence:')
-print(xi)
-print('Ground truth:')
-print(yi)
-print('Prediction:')
-print(session.run(pred, {X_int: [xi], lengths: [len(xi)]})[0])
+# feed = {X_int: X_val, Y_int: Y_val, lengths: sequence_length}
+# accuracy_ = session.run(accuracy, feed)
+# print(f'Validation accuracy: {accuracy_}.')
+
+# sys.exit()
+
+# # Shows first task and corresponding prediction
+# xi = X_val[0, 0: lengths_val[0]]
+# yi = Y_val[0, 0: lengths_val[0]]
+# print('Sequence:')
+# print(xi)
+# print('Ground truth:')
+# print(yi)
+# print('Prediction:')
+# print(session.run(pred, {X_int: [xi], lengths: [len(xi)]})[0])
 
 session.close()
 
